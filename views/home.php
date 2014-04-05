63b1b56c652fefe1206ae47942e9d2dc171d6d80
Colby Rabideau <me@colbyr.com>
2014-04-05 16:35:58 -0700

    a grid!

--- views/home.php
+++ views/home.php
@@ -1,14 +1,16 @@
-<h1><?= $data ?></h1>
+<div class="pure-u-5-5">
+  <form class="pure-form" method="GET">
+    <input name="language" placeholder="language" type="text" value="<?= getInput('language') ?>" />
+    <button class="pure-button pure-button-primary" type="submit">
+      Find Issues
+    </button>
+  </form>
+</div>
 
-<form class="pure-form" method="GET">
-  <input name="language" type="text" value="<?= getInput('language') ?>" />
-  <button class="pure-button pure-button-primary" type="submit">
-    Find Issues
-  </button>
-</form>
-
-<ul>
-  <? foreach ($data as $issue) { ?>
-    <li><a href="<?= $issue->url ?>"><?= $issue->title ?></a></li>
-  <? } ?>
-</ul>
+<div class="pure-u-5-5">
+  <ul class="unstyled">
+    <? foreach ($data as $issue) { ?>
+      <li><a href="<?= $issue->url ?>"><?= $issue->title ?></a></li>
+    <? } ?>
+  </ul>
+</div>
--- views/layout.php
+++ views/layout.php
@@ -4,10 +4,29 @@
   <head>
     <title>Low Hanging Fruit</title>
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/pure/0.4.2/pure-min.css" />
+
+    <style type="text/css">
+      .page {
+        margin: 0 auto;
+        max-width: 720px;
+      }
+      ol.unstyled,
+      ul.unstyled {
+        list-style: none;
+        padding-left: 0;
+      }
+    </style>
   </head>
 
   <body>
-    <? require "../views/$path.php" ?>
+    <div class="page pure-g-r">
+      <header class="pure-u-5-5">
+        <h1>Low Hanging Fruit</h1>
+      </header>
+
+      <? require "../views/$path.php" ?>
+
+    </div>
   <body>
 
 </html>
