238d0d556aab63166377f9c92f31f51677adb295
Colby Rabideau <me@colbyr.com>
2014-04-05 17:23:34 -0700

    some style

--- views/home.php
+++ views/home.php
@@ -8,9 +8,17 @@
 </div>
 
 <div class="pure-u-5-5">
+  <h2>
+    <?= ucwords(getInput('language', 'all')) ?> Issues (<?= count($data) ?>)
+  </h2>
   <ul class="unstyled">
     <? foreach ($data as $issue) { ?>
-      <li><a href="<?= $issue->url ?>"><?= $issue->title ?></a></li>
+      <li>
+        <a class="issue" href="<?= $issue->url ?>" target="_blank">
+          <h3 class="title"><?= $issue->title ?></h3>
+          <p class="description">This is a description</p>
+        </a>
+      </li>
     <? } ?>
   </ul>
 </div>
--- views/layout.php
+++ views/layout.php
@@ -6,9 +6,49 @@
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/pure/0.4.2/pure-min.css" />
 
     <style type="text/css">
+      a {
+        color: #0078e7;
+        text-decoration: none;
+      }
+      form,
+      header {
+        text-align: center;
+      }
+      .issue {
+        border: 1px solid #eee;
+        border-radius: 4px;
+        color: inherit;
+        display: block;
+        margin-bottom: 10px;
+        padding: 15px;
+      }
+      .issue:hover {
+        border-color: #0078e7;
+      }
+      .issue:visited {
+      }
+        .issue .description {
+          background-color: #eee;
+          margin-bottom: 0;
+          padding: 5px;
+        }
+        .issue .title {
+          margin-bottom: 15px;
+          margin-top: 0;
+        }
+        .issue:active .title,
+        .issue:active:visited .title,
+        .issue:hover .title,
+        .issue:hover:visited .title {
+          color: #0078e7;
+        }
+        .issue:visited .title {
+          color: #999;
+        }
       .page {
         margin: 0 auto;
-        max-width: 720px;
+        max-width: 740px;
+        padding: 0 10px;
       }
       ol.unstyled,
       ul.unstyled {
